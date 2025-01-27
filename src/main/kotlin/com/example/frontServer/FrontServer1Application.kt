b7f75511c7186121d61ebaaf32dbbb83e960215f
zen <didehdrb21@naver.com>
2025-01-27 20:57:19 +0900

    spring-profile-group

--- src/main/kotlin/com/example/frontServer/FrontServer1Application.kt
+++ src/main/kotlin/com/example/frontServer/FrontServer1Application.kt
@@ -2,6 +2,8 @@ package com.example.frontServer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.EnvironmentAware
+import org.springframework.core.env.Environment
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
