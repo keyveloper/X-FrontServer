ffb8e223d281908bd5cbe5f5a8a39adff16f0661
zen <didehdrb21@naver.com>
2025-02-12 22:06:56 +0900

    add prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,6 +92,10 @@ dependencies {
 
     // actuator
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // prometheus
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
 }
 
 kotlin {
