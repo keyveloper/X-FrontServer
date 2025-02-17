b1bcc1189b0204e7efa9cfd09a8cca9940042d69
zen <didehdrb21@naver.com>
2025-02-17 15:40:49 +0900

    jenkin build test controller

--- src/main/kotlin/com/example/frontServer/controller/TestController.kt
+++ src/main/kotlin/com/example/frontServer/controller/TestController.kt
@@ -1,6 +1,7 @@
 package com.example.frontServer.controller
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,5 +11,10 @@ class TestController {
     @GetMapping("/loafBalancing")
     fun loadBalancing() {
         logger.info {"Load Balancing !!!"}
+    }
+
+    @GetMapping("/jenkins")
+    fun jenkins(): ResponseEntity<String> {
+        return ResponseEntity.ok().body("Hello Jenkins build!")
     }
 }
